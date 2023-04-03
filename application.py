074b9b24154781e07bb0ae385b216c39e1c80cb3
madhu sudan mandal <52095904+theAFKhero@users.noreply.github.com>
2023-04-03 12:49:50 +0530

    Update application.py

--- application.py
+++ application.py
@@ -87,4 +87,4 @@ def review():
         return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host= "localhost", port = 5400)
+    app.run(host='127.0.0.1', port=8000, debug=True)
